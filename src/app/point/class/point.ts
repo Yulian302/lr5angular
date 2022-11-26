079b6ae3488c6a1ee5ebae0c01de58441fa9205f
Yulian302 <bohomolyulian3022003@gmail.com>
2022-11-26 10:22:49 +0200

    2 files modified, 2 added

---
+++ src/app/point/class/point.ts
@@ -0,0 +1,10 @@
+export class Point {
+  x: number;
+  y: number;
+  belong: boolean;
+  constructor(x: number, y: number, b: boolean) {
+    this.x = x;
+    this.y = y;
+    this.belong = b;
+  }
+}
