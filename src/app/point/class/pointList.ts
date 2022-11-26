6ad4e1950b08131f8f0e2d5308caf907f8839783
Yulian302 <bohomolyulian3022003@gmail.com>
2022-11-26 10:31:57 +0200

    1 file modified, 1 added

---
+++ src/app/point/class/pointList.ts
@@ -0,0 +1,14 @@
+import { Point } from './point';
+export class PointList {
+  p: Point[] = [];
+  private Belongs(x: number, y: number, r: number): boolean {
+    if (x >= -r && x <= r && y >= -r && y <= r && x * x + y * y >= r * r) {
+      return true;
+    } else return false;
+  }
+  addPoint(x: number, y: number, r: number) {
+    let b = this.Belongs(x, y, r);
+    let p = new Point(x, y, b);
+    this.p.push(p);
+  }
+}
---
+++ src/app/point/pointcomponent.ts
@@ -0,0 +1,2 @@
+import { Component, OnInit } from '@angular/core';
+import { PointList } from './class/pointList';
